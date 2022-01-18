30aacacb3fa9b0b7d4c26ddfb92cadc2a5dea2e8
Wilmer Negron <wnegron@helpdesk.peru>
2022-01-18 17:59:16 -0500

    Version 1.0

---
+++ Web_Conformidad/Controllers/HomeController.cs
@@ -0,0 +1,186 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Web_Conformidad.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public string GeneraToken()
+        {
+            var grant_type = "password";
+            var scope = "https://api.sunat.gob.pe";
+            var client_id = "97dee99e-984b-4369-a72e-fb03a6c3defa";
+            var client_secret = "caWpscwQt8zFaK5t1kgftg==";
+            var username = "20601647649EFACT001";
+            var password = "factu2018";
+
+            var request = (HttpWebRequest)WebRequest.Create("https://api-seguridad.sunat.gob.pe/v1/clientessol/" + client_id + "/oauth2/token/");
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.SystemDefault | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+
+            var postData = "grant_type=" + grant_type + "&scope=" + scope + "&client_id=" + client_id + "&client_secret=" + client_secret + "&username=" + username + "&password=" + password;
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            string resp = "";
+            try
+            {
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+                string responseString = "";
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null)
+                        {
+                            return resp;
+                        }
+
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            responseString = objReader.ReadToEnd();
+                        }
+                    }
+                }
+
+                var token = new JavaScriptSerializer().Deserialize<dynamic>(responseString);
+                resp = token["access_token"];
+
+                
+            }
+            catch (ArgumentException ae)
+            {
+                resp = "Error: " + ae.Message;
+            }
+            catch (ProtocolViolationException pve)
+            {
+                resp = "Error: " + pve.Message;
+            }
+            catch (InvalidOperationException ioe)
+            {
+                resp = "Error: " + ioe.Message;
+            }
+            catch (NotSupportedException nse)
+            {
+                resp = "Error: " + nse.Message;
+            }
+
+            
+            return resp;
+
+        }
+
+        public object ConsultaDocumentosPND()
+        {
+            string token = GeneraToken();
+
+
+            var client = new RestClient("https://api-cpe.sunat.gob.pe/v1/contribuyente/controlcpe/comprobantes?indFechaFiltro=FE&codCpe=01&fecInicio=2021-12-01&fecFin=2022-12-31&numPag=1&numRegPag=50&codEstado=01&codTipTransaccion=&codMoneda=PEN&numSerie=&numCpe=&numRuc=&codTipoDocAdqui=&numDocAdqui=&indContribuyente=C");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", "Bearer " + token);
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            return response.Content;
+        }
+
+        public object ConsultaTickets()
+        {
+            string token = GeneraToken();
+
+
+            var client = new RestClient("https://api-cpe.sunat.gob.pe/v1/contribuyente/controlcpe/enviosmasivo?indTipoRegistro=01&fecEnvioInicial=2022-01-01&fecEnvioFinal=2022-01-10&numPag=1&numRegPag=20&codTicketMasivo=");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", "Bearer " + token);
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            return response.Content;
+        }
+
+        public object EnviaConformidad(string rutaArchivo)
+        {
+            string token = GeneraToken();
+
+            var client = new RestClient("https://api-cpe.sunat.gob.pe/v1/contribuyente/controlcpe/enviosmasivo/01");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", "Bearer " + token);
+            request.AddFile("archivo", @rutaArchivo);
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            return response.Content;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetToken()
+        {
+            string token = GeneraToken();
+            return Json(token, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GetDocumntosPND()
+        {
+            var objRes = ConsultaDocumentosPND();
+
+            return Json(objRes, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GetTicketsPND()
+        {
+            var objRes = ConsultaTickets();
+
+            return Json(objRes, JsonRequestBehavior.AllowGet);
+        }
+       
+        [HttpPost]
+        public JsonResult PostConformidad(string rutaArchivo)
+        {
+            var objRes = EnviaConformidad("");
+
+            return Json(objRes, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Web_Conformidad/Models/Comprobantes.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Conformidad.Models
+{
+    public class Comprobantes
+    {
+
+        public class Rootobject
+        {
+            public int cntTotalReg { get; set; }
+            public int numPag { get; set; }
+            public int numRegPag { get; set; }
+            public int numDiasAtencionMax { get; set; }
+            public int numDiasAtencionMaxAdic { get; set; }
+            public Numeropagina[] numeroPaginas { get; set; }
+            public Comprobante[] comprobantes { get; set; }
+        }
+
+        public class Numeropagina
+        {
+            public int numPagFront { get; set; }
+        }
+
+        public class Comprobante
+        {
+            public string codCpe { get; set; }
+            public string numSerie { get; set; }
+            public int numCpe { get; set; }
+            public string codMoneda { get; set; }
+            public Datosemisor datosEmisor { get; set; }
+            public Datosreceptor datosReceptor { get; set; }
+            public Procedenciaindividual procedenciaIndividual { get; set; }
+            public Procedenciamasiva procedenciaMasiva { get; set; }
+            public DateTime fecEmision { get; set; }
+            public object fecVencimiento { get; set; }
+            public DateTime fecRegistro { get; set; }
+            public DateTime fecPuestaDisposicion { get; set; }
+            public string codTipTransaccion { get; set; }
+            public int numDiasAtencion { get; set; }
+            public string indEstadoCpe { get; set; }
+            public string indProcedencia { get; set; }
+            public string indTituloGratuito { get; set; }
+            public object datosRHE { get; set; }
+            public Transaccione[] transacciones { get; set; }
+            public Datodocrelacionado datoDocRelacionado { get; set; }
+            public Eventosmovimiento[] eventosMovimiento { get; set; }
+            public Auditoria auditoria { get; set; }
+            public object notasCpe { get; set; }
+        }
+
+        public class Datosemisor
+        {
+            public string numRuc { get; set; }
+            public string desRazonSocialEmis { get; set; }
+        }
+
+        public class Datosreceptor
+        {
+            public string codDocIdeRecep { get; set; }
+            public string numDocIdeRecep { get; set; }
+            public string desRazonSocialRecep { get; set; }
+        }
+
+        public class Procedenciaindividual
+        {
+            public float? mtoSubTotalVentas { get; set; }
+            public object mtoAnticipos { get; set; }
+            public object mtoDtos { get; set; }
+            public float? mtoValorVenta { get; set; }
+            public object mtoISC { get; set; }
+            public float? mtoIGV { get; set; }
+            public object mtoICBPER { get; set; }
+            public object mtoOtrosCargos { get; set; }
+            public object mtoOtrosTributos { get; set; }
+            public float? mtoImporteTotal { get; set; }
+        }
+
+        public class Procedenciamasiva
+        {
+            public object mtoDtoGlobalAfecBI { get; set; }
+            public float? mtoTotalValVentaGrabado { get; set; }
+            public float? mtoTotalValVentaInafecto { get; set; }
+            public float? mtoTotalValVentaExonerado { get; set; }
+            public float? mtoTotalValVentaGratuito { get; set; }
+            public object mtoTotalValVentaExportacion { get; set; }
+            public object mtoDtoGlobalNoAfecBI { get; set; }
+            public object mtoTotalDtos { get; set; }
+            public object mtoSumOtrosTributos { get; set; }
+            public object mtoSumOtrosCargos { get; set; }
+            public object mtoSumISC { get; set; }
+            public float? mtoSumIGV { get; set; }
+            public object mtoSumICBPER { get; set; }
+            public float? mtoTotalAnticipo { get; set; }
+            public float? mtoImporteTotal { get; set; }
+        }
+
+        public class Datodocrelacionado
+        {
+            public object codTipNota { get; set; }
+            public object desMotivo { get; set; }
+            public object documentosModifica { get; set; }
+            public object documentosRelacionado { get; set; }
+        }
+
+        public class Auditoria
+        {
+            public string codUsuRegis { get; set; }
+            public DateTime fecRegis { get; set; }
+            public string codUsuModif { get; set; }
+            public DateTime fecModif { get; set; }
+        }
+
+        public class Transaccione
+        {
+            public string codCpeTransaccion { get; set; }
+            public string numSerieTransaccion { get; set; }
+            public int numCpeTransaccion { get; set; }
+            public float mtoPagoPendiente { get; set; }
+            public DateTime fecPlazoPago { get; set; }
+            public DateTime fecEmisionTransaccion { get; set; }
+            public int numCuotas { get; set; }
+            public Cuota[] cuotas { get; set; }
+        }
+
+        public class Cuota
+        {
+            public string numCuota { get; set; }
+            public float mtoCuota { get; set; }
+            public DateTime fecVencimiento { get; set; }
+            public object pagos { get; set; }
+        }
+
+        public class Eventosmovimiento
+        {
+            public int numMovimiento { get; set; }
+            public DateTime fecModificacion { get; set; }
+            public object desComentario { get; set; }
+            public object indProcedencia { get; set; }
+            public object desUsuRegis { get; set; }
+            public Condicioncomprobante condicionComprobante { get; set; }
+            public object disconformidad { get; set; }
+            public object subsanaciones { get; set; }
+            public object baja { get; set; }
+        }
+
+        public class Condicioncomprobante
+        {
+            public string codEstadoCondicion { get; set; }
+            public string codCondicion { get; set; }
+            public string codMarcaCondicion { get; set; }
+            public string codEfectoCredFiscal { get; set; }
+            public string codEfectoFactoring { get; set; }
+            public string codEvento { get; set; }
+        }
+
+    }
+}
